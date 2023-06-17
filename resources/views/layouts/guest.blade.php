9f1872c5471a074725983978709e01e49548b13c
JUANCAMILO2001 <juan.camilorodrigez@gmail.com>
2023-06-16 22:55:48 -0500

    Creacion de modal para productos

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,6 +19,7 @@
     <meta name="theme-color" content="#6777ef"/>
     <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
+    @yield('style')
 </head>
 <body>
 <!-- lodaer  -->
--- resources/views/user/products/index.blade.php
+++ resources/views/user/products/index.blade.php
@@ -19,25 +19,42 @@
                         </div>
                         @if(auth()->check())
                             @foreach($products as $product)
-                                <!-- hero-menu-item-->
+                                <!-- hero-menu-item -->
                                 <div class="hero-menu-item" style="height: 112px;">
-                                    <a href="/public/{{Storage::url($product->imagen)}}" class="hero-menu-item-img image-popup">
-                                        <img src="/public/{{Storage::url($product->imagen)}}" alt="">
+                                    <a href="/public/{{ Storage::url($product->imagen) }}" class="hero-menu-item-img image-popup">
+                                        <img src="/public/{{ Storage::url($product->imagen) }}" alt="">
                                     </a>
                                     <div class="hero-menu-item-title fl-wrap">
-                                        <h6>{{$product->name}}</h6>
+                                        <h6>{{ $product->name }}</h6>
                                         <div class="hmi-dec" style="left: 162px;"></div>
-                                        <span class="hero-menu-item-price">$ {{number_format(intval($product->pay))}}</span>
+                                        <span class="hero-menu-item-price">$ {{ number_format(intval($product->pay)) }}</span>
+                                        <div class="add_cart" title="Añadir al carrito">
+                                            <a href="#" class="hero__cta" data-toggle="modal" data-target="#modal_{{ $loop->iteration }}">Añadir</a>
+                                        </div>
+                                    </div>
+                                    <div class="hero-menu-item-details">
+                                        <p>{{ $product->description }}</p>
+                                    </div>
+                                </div>
+                                <!-- hero-menu-item end -->
+
+                                <!-- Modal -->
+                                <section class="modal" id="modal_{{ $loop->iteration }}">
+                                    <div class="modal__container">
+                                        <img class="modal__img" src="/public{{ Storage::url($product->imagen) }}" alt="">
+                                        <h6>{{ $product->name }}</h6>
+                                        <span class="hero-menu-item-price">$ {{ number_format(intval($product->pay)) }}</span>
                                         <form action="{{ route('add_to_cart', $product->id) }}" method="post" id="add_cart_{{ $loop->iteration }}">
                                             @csrf
                                         </form>
-                                        <div class="add_cart" title="Añadir al carrito"><a onclick="document.getElementById('add_cart_{{ $loop->iteration }}').submit()">Añadir</a></div>
+                                        <div class="add_cart" title="Añadir al carrito">
+                                            <a onclick="document.getElementById('add_cart_{{ $loop->iteration }}').submit()">Añadir</a>
+                                        </div>
+                                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                                        <a style="background-color: green;color: white;height: 23px;width: 75px;border-radius: 5px;" onclick="document.getElementById('add_cart_{{ $loop->iteration }}').submit()">Añadir</a>
+                                        <a href="#" class="modal__close">Cerrar producto</a>
                                     </div>
-                                    <div class="hero-menu-item-details">
-                                        <p>{{$product->description}}</p>
-                                    </div>
-                                </div>
-                                <!-- hero-menu-item end-->
+                                </section>
                             @endforeach
                         @else
                             @foreach($products as $product)
@@ -66,4 +83,106 @@
                 </div>
             </section>
         </div>
+@endsection
+@section('style')
+    <style>
+        .modal{
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #111111bd;
+            display: flex;
+            opacity: 0;
+            pointer-events: none;
+            transition: opacity .6s .9s;
+            --transform: translateY(-100vh);
+            --transition: transform .8s;
+        }
+        .modal--show{
+            opacity: 1;
+            pointer-events: unset;
+            transition: opacity .6s;
+            --transform: translateY(0);
+            --transition: transform .8s .8s;
+            z-index: 6;
+        }
+
+        .modal__container{
+            margin: auto;
+            width: 90%;
+            max-width: 600px;
+            max-height: 90%;
+            background-color: #fff;
+            border-radius: 6px;
+            padding: 3em 2.5em;
+            display: grid;
+            gap: 1em;
+            place-items: center;
+            grid-auto-columns: 100%;
+            transform: var(--transform);
+            transition:var(--transition);
+        }
+
+        .modal__title{
+            font-size: 2.5rem;
+        }
+
+        .modal__paragraph{
+            margin-bottom: 10px;
+        }
+
+        .modal__img{
+            width: 90%;
+            max-width: 300px;
+        }
+
+        .modal__close{
+            text-decoration: none;
+            color: #fff;
+            background-color: #F26250;
+            padding: 1em 3em;
+            border: 1px solid ;
+            border-radius: 6px;
+            display: inline-block;
+            font-weight: 300;
+            transition: background-color .3s;
+        }
+
+        .modal__close:hover{
+            color: #F26250;
+            background-color: #fff;
+        }
+        @media (max-width:800px) {
+            .modal__container{
+                padding: 2em 1.5em;
+            }
+
+            .modal__title{
+                font-size: 2rem;
+            }
+        }
+    </style>
+@endsection
+@section('js')
+    <script>
+        const openModal = document.querySelectorAll('.hero__cta');
+        const modals = document.querySelectorAll('.modal');
+        const closeModal = document.querySelectorAll('.modal__close');
+
+        openModal.forEach((button, index) => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+                modals[index].classList.add('modal--show');
+            });
+        });
+
+        closeModal.forEach((button, index) => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+                modals[index].classList.remove('modal--show');
+            });
+        });
+    </script>
 @endsection
