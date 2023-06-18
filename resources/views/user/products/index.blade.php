b2dcf6a1f4d1fcaef1293b24a6eb4c0512a4bcb8
JUANCAMILO2001 <juan.camilorodrigez@gmail.com>
2023-06-18 00:34:31 -0500

    agregando modal para carrito de compra

--- resources/views/user/products/index.blade.php
+++ resources/views/user/products/index.blade.php
@@ -41,20 +41,38 @@
                                 <!-- Modal -->
                                 <section class="modal" id="modal_{{ $loop->iteration }}">
                                     <div class="modal__container">
-                                        <div>
-                                            <img class="modal__img" src="/public{{ Storage::url($product->imagen) }}" alt="">
+                                        <div class="close-modal">
+                                            <span class="btn-close-modal">x</span>
                                         </div>
-                                        <div>
-                                            <h6>{{ $product->name }}</h6>
-                                        <span class="hero-menu-item-price">$ {{ number_format(intval($product->pay)) }}</span>
-                                        <form action="{{ route('add_to_cart', $product->id) }}" method="post" id="add_cart_{{ $loop->iteration }}">
-                                            @csrf
-                                        </form>
-                                        
-                                        <textarea name="" id="" cols="30" rows="10"></textarea>
-                                        <a style="background-color: green;color: white;height: 23px;width: 75px;border-radius: 5px;" onclick="document.getElementById('add_cart_{{ $loop->iteration }}').submit()">Añadir</a>
+                                        <img class="modal__img" src="/public{{ Storage::url($product->imagen) }}" alt="">
+                                        <div class="gird-modal-content">
+                                            <div class="text-modal">
+                                                <p class="title-modal">{{ $product->name }}</p>
+                                                <p>{{ $product->description }}</p>
+                                                <p class="precio-modal">$ {{ number_format(intval($product->pay)) }}</p>
+                                            </div>
+                                            <form action="{{ route('add_to_cart', $product->id) }}" method="post" id="add_cart_{{ $loop->iteration }}">
+                                                @csrf
+                                            </form>
+                                            <div class="text-modal coment-modal">
+                                                <p class="sub-title-modal">Comentario para tu pedido:</p>
+                                                <textarea name="" id="" cols="30" rows="6"></textarea>
+                                            </div>
+                                            <div class="text-modal ">
+                                                <p class="sub-title-modal">Cantidad</p>
+                                                <div class="center-quantity">
+                                                    <div class="number-input">
+                                                        <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()" ></button>
+                                                        <input required class="quantity" min="0" name="quantity" value="0" type="number">
+                                                        <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus"></button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="btn-modal">
+                                                <a class="modal__agregar" onclick="document.getElementById('add_cart_{{ $loop->iteration }}').submit()">Añadir</a>
+                                                <a class="modal__close">Cancelar</a>
+                                            </div>
                                         </div>
-                                        <a href="#" class="modal__close">Cerrar producto</a>
                                     </div>
                                 </section>
                             @endforeach
@@ -113,14 +131,14 @@
 
         .modal__container{
             margin: auto;
-            width: 90%;
-            max-width: 600px;
+            width: 100%;
+            max-width: 750px;
             max-height: 90%;
             background-color: #fff;
             border-radius: 6px;
             padding: 3em 2.5em;
-            display: grid;
-            gap: 1em;
+            display: flex;
+            gap: 3em;
             place-items: center;
             grid-auto-columns: 100%;
             transform: var(--transform);
@@ -138,23 +156,173 @@
         .modal__img{
             width: 90%;
             max-width: 300px;
+            border-radius: 15px;
         }
 
         .modal__close{
             text-decoration: none;
-            color: #fff;
-            background-color: #F26250;
+            color: #C19D60;
+            font-weight: bold;
+            background-color: #fff;
             padding: 1em 3em;
-            border: 1px solid ;
+            border: 1px solid #C19D60;
             border-radius: 6px;
             display: inline-block;
-            font-weight: 300;
             transition: background-color .3s;
         }
 
         .modal__close:hover{
-            color: #F26250;
+            color: black;
+            font-weight: bold;
+            background-color: #C19D60;
+        }
+
+        .modal__agregar{
+            text-decoration: none;
+            color: #C19D60;
+            font-weight: bold;
             background-color: #fff;
+            padding: 1em 3em;
+            border: 1px solid #C19D60;
+            border-radius: 6px;
+            display: inline-block;
+            transition: background-color .3s;
+            margin-left: 14px;
+            margin-right: 21px;
+        }
+
+        .modal__agregar:hover{
+            color: black;
+            font-weight: bold;
+            background-color: #C19D60;
+        }
+
+
+        .close-modal{
+            position: absolute;
+            margin-right: 100px;
+            margin-top: -367px;
+            margin-left: 703px;
+        }
+        .btn-close-modal{
+            background-color: #292929;
+            padding: 0px 6px 2px 7px;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 17px;
+            font-weight: bold;
+            cursor: pointer;
+            color: #fff;
+        }
+        .gird-modal-content{
+            display: grid;
+            width: 100%;
+            align-items: start!important;
+            max-height: 267px;
+            overflow-y: scroll;
+        }
+        .title-modal{
+            font-weight: bold;
+            font-size: 25px;
+            letter-spacing: 3px;
+        }
+        .precio-modal{
+            font-weight: bold;
+            font-size: 15px;
+        }
+        .sub-title-modal{
+            font-size: 15px;
+            font-weight: bold;
+            letter-spacing: 2px;
+        }
+
+        .text-modal{
+            text-align: left;
+        }
+        .coment-modal textarea{
+            width: 97%;
+            border-top-color: transparent;
+            border-left-color: transparent;
+            border-right-color: transparent;
+        }
+        .coment-modal textarea:focus{
+            border-color: #5b5b5b;
+        }
+        .btn-modal{
+            display: flex;
+            margin-top: 15px;
+        }
+
+        .center-quantity{
+            display: flex;
+            justify-content: center;
+            width: 100%;
+        }
+
+        input[type="number"] {
+            -webkit-appearance: textfield;
+            -moz-appearance: textfield;
+            appearance: textfield;
+        }
+
+        input[type=number]::-webkit-inner-spin-button,
+        input[type=number]::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+        }
+
+        .number-input {
+            border: 2px solid #5e646a;
+            border-radius: 5px;
+            display: inline-flex;
+        }
+
+        .number-input,
+        .number-input * {
+            box-sizing: border-box;
+        }
+
+        .number-input button {
+            outline:none;
+            -webkit-appearance: none;
+            background-color: transparent;
+            border: none;
+            align-items: center;
+            justify-content: center;
+            width: 2rem;
+            height: 2rem;
+            cursor: pointer;
+            margin: 0;
+            position: relative;
+            color: #5e646a;
+        }
+
+        .number-input button:before,
+        .number-input button:after {
+            display: inline-block;
+            position: absolute;
+            content: '';
+            width: 1rem;
+            height: 2px;
+            background-color: #5e646a;
+            transform: translate(-50%, -50%);
+        }
+        .number-input button.plus:after {
+            transform: translate(-50%, -50%) rotate(90deg);
+        }
+
+        .number-input input[type=number] {
+            font-family: sans-serif;
+            max-width: 2rem;
+            padding: .5rem;
+            border: solid #5e646a;
+            border-width: 0 2px;
+            font-size: 1rem;
+            height: 2rem;
+            font-weight: bold;
+            text-align: center;
+            color: #5e646a;
         }
         @media (max-width:800px) {
             .modal__container{
@@ -172,6 +340,7 @@
         const openModal = document.querySelectorAll('.hero__cta');
         const modals = document.querySelectorAll('.modal');
         const closeModal = document.querySelectorAll('.modal__close');
+        const closeModal2 = document.querySelectorAll('.btn-close-modal');
 
         openModal.forEach((button, index) => {
             button.addEventListener('click', (e) => {
@@ -181,6 +350,12 @@
         });
 
         closeModal.forEach((button, index) => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+                modals[index].classList.remove('modal--show');
+            });
+        });
+        closeModal2.forEach((button, index) => {
             button.addEventListener('click', (e) => {
                 e.preventDefault();
                 modals[index].classList.remove('modal--show');
